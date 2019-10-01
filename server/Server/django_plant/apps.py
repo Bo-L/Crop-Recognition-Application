ea9f8ccaa6c2c023802486bddad114a11a1f258a
刘博 <liubo@bogedeMacBook-Pro.local>
2019-10-02 02:36:24 +0800

    delete

--- server/Server/django_plant/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoPlantConfig(AppConfig):
-    name = 'django_plant'
--- server/Server/django_plant/utils/yaml_read.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-def parse():
-    with open('./django_plant/config.yaml','rb') as file:
-        config=yaml.load(file)
-        return config
